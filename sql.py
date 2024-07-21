0d838ab56268f835fec6a0e96477cc473a3c2074
Jreddy0077 <127132793+Jreddy0077@users.noreply.github.com>
2024-07-21 12:33:08 +0530

    Update sql.py

--- sql.py
+++ sql.py
@@ -1,16 +1,5 @@
-# streamlit_app.py
 
 import streamlit as st
-
-# Initialize connection.
-conn = st.connection('test', type='sql')
-
-# Perform query.
-df = conn.query('SELECT * from user;', ttl=600)
-
-# Print results.
-for row in df.itertuples():
-    st.write(f"{row.name} has a :{row.pet}:")import streamlit as st
 import pandas as pd
 from sqlalchemy import create_engine
 
